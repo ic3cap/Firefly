b73251c85d7b584ed8813720f86f9c79f23e2260
ic3cap <112591632+ic3cap@users.noreply.github.com>
2023-08-31 05:57:38 -0400

    mongodb connector

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { Client, IntentsBitField, Collection } = require('discord.js');
 const { readdirSync } = require('fs');
+const { default: mongoose } = require('mongoose');
 require('dotenv').config();
 
 const client = new Client({
@@ -12,6 +13,16 @@ const client = new Client({
 });
 
 client.commands = new Collection();
+
+(async () => {
+    await mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log('Successfully connected to MongoDB!');
+        })
+        .catch(err => {
+            console.log(`Error connecting to MongoDB!\nError: ${err}`);
+        });
+})();
 
 client.once('ready', c => {
     console.log(`Logged in as ${c.user.tag}!`);
@@ -32,7 +43,31 @@ client.on('interactionCreate', async interaction => {
 })
 
 /*
-mongodb+srv://fireflyadmin:<password>@firefly-discord-bot.hlh6oby.mongodb.net/?retryWrites=true&w=majority
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://fireflyadmin:<password>@firefly-discord-bot.hlh6oby.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 */
 
 client.login(process.env.TOKEN);
